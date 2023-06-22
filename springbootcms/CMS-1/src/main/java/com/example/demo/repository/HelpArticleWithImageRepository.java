75c23b8eab0143e51236f66255191ebd386b12cc
Greeshma Thattepally <50731579+greeshmathattepally@users.noreply.github.com>
2023-06-22 10:11:57 +0530

    Delete springbootcms/CMS-1 directory
    some changes are needed

--- springbootcms/CMS-1/src/main/java/com/example/demo/repository/HelpArticleWithImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.demo.model.HelpArticleWithImage;
-
-public interface HelpArticleWithImageRepository extends MongoRepository<HelpArticleWithImage, Long>{
-	HelpArticleWithImage findTopByOrderByIdDesc();
-}
